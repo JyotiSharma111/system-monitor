cbf88e7c1df0c774845e31ed484cf3d610868099
Jyoti Sharma <minikanva@gmail.com>
2026-05-23 14:12:09 -0500

    Initial commit - system monitor API

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from psutil import cpu_percent, virtual_memory, disk_usage
+
+app=FastAPI()
+
+@app.get("/")
+def root():
+    return {"status":"ok"}
+
+@app.get("/info")
+def info():
+    return {"message":"Your app info is good"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+
+@app.get("/metrics")
+def metrics():
+    return{
+        "cpu":{"usage_percent": cpu_percent(interval=1)},
+        "memory":{
+            "total_gb": round(virtual_memory().total/(1024**3)),
+            "available_gb": round(virtual_memory().available/(1024**3)),
+            "percent_used": virtual_memory().percent
+
+        }
+                  ,
+        "disk":{
+            "total_gb": round(disk_usage("/").total/(1024**3)),
+            "available_gb": round(disk_usage("/").free/(1024**3)),
+            "percent_used":disk_usage("/").percent
+
+        }
+    }
